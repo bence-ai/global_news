e687a5277bfe2892f7fb74aa0dae16342d537861
feherdaniel <feherdaniel2010@gmail.com>
2021-08-25 13:48:05 +0200

    style: add basic styled components

--- global_news/src/Components/homepage.js
+++ global_news/src/Components/homepage.js
@@ -2,22 +2,57 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import styled from 'styled-components';
 
-
-
-
-
 function MainNewsList() {
-    
-    const TopBox = styled.div`
-        display: box;
-        justify-content: center;
-        align-items: center;
-        color: blue;
+    const Main = styled.div`
+        position: relative;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-column-gap: 32px;
+        border: 1px solid transparent;
+        border-top: 1px solid #DADCE0;
+        border-bottom: 1px solid #DADCE0;
+        overflow: hidden;
+        background-color: #E3E7F1;
     `;
+
+    const Li = styled.li`
+        background-color: whitesmoke;
+        padding: 12px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        min-height: 90px;
+        height: 20%;
+        border-radius: 10px;
+    `;
+
+    const Card = styled.div`
+        position: relative;
+        
+        z-index: 2;
+        padding: 16px 0;
+        background-color: #E3E7F1;
+        grid-row: 1 / span 2;
+    `;
+
+    const Card2 = styled.div`
+        position: relative;
+        
+        z-index: 2;
+        padding: 16px 37px 0 0;
+        background-color: #E3E7F1;
+        grid-column: 2 / span 2;
+        border-left: 3px solid whitesmoke;
+    `;
+
+    const A = styled.a`
+        text-decoration: none;
+        color: black;
+    `
 
     const [mainNews, setMainNews] = useState([]);
     const [leftBar, setLeftbar] = useState([]);
-    const url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=6978740ff320434a9ef38606f359cbb2"
+    const url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=6978740ff320434a9ef38606f359cbb2&pagesize=10"
     const urlLeft = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=6978740ff320434a9ef38606f359cbb2";
 
     useEffect(() => {
@@ -26,30 +61,32 @@ function MainNewsList() {
             setLeftbar(res.data.articles)
         })
     }, [urlLeft])
-    console.dir(leftBar);
-    console.log("left bar  set");
-
+   
     useEffect(() => {
         axios.get(url)
             .then(res => {
-                console.dir(res)
-                console.log("before set")
                 setMainNews(res.data.articles);
             })
     }, [url])
-    console.dir(mainNews);
-    console.log("after set");
     
     return(
-        
-        <div style={{backgroundColor : 'whitesmoke'}} >
-            <TopBox>lets see</TopBox>
-            <ul>
-                {
-                mainNews.map(news => <li key={news.source.id}> {news.author}</li>)
-                }
-            </ul>
-        </div>
+        <Main>
+            <Card>
+                <ul>
+                    {
+                    mainNews.map(news => <Li key={news.title}> <A href={news.url}>{news.title}</A></Li>)
+                    }
+                </ul>
+            </Card>
+            <Card2>
+                <ul>
+                    {
+                    leftBar.map(news => <Li key={news.title}> <A href={news.url}>{news.title}</A></Li>)
+                    }
+                </ul>
+            </Card2>
+        </Main>
     )
 }
+
 export default MainNewsList;
