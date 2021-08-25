9159bb87de2ec7148f2838ea4b3507e5776463a5
Farkas Timot <farkas.timot@gmail.com>
2021-08-25 09:43:01 +0200

    feat: added routes

--- global_news/src/App.js
+++ global_news/src/App.js
@@ -1,10 +1,19 @@
 import Homepage from './Components/Homepage'
+import FilteredNews from './Components/FilteredNews';
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Homepage/>
+      <BrowserRouter>
+          
+          <Switch>
+            <Route path="/" component={Homepage} exact />
+            <Route path="/search" render={(props) => (<FilteredNews language={'en'} searchTerm={'tesla'} />)} />
+            <Redirect to="/" />
+          </Switch>
+        </BrowserRouter>
     </div>
   );
 }
