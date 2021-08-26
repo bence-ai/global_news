687f306887fe019050df3a819a14576d83e3b3d3
feherdaniel <feherdaniel2010@gmail.com>
2021-08-26 12:11:56 +0200

    style: change style

--- global_news/src/Components/homepage.js
+++ global_news/src/Components/homepage.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 function MainNewsList() {
     const Main = styled.div`
@@ -9,51 +9,59 @@ function MainNewsList() {
         grid-template-columns: 1fr 1fr 1fr;
         grid-column-gap: 32px;
         border: 1px solid transparent;
-        border-top: 1px solid #DADCE0;
+        border-top: 1px solid #433b1e;
         border-bottom: 1px solid #DADCE0;
         overflow: hidden;
-        background-color: #E3E7F1;
+        background-color: #433b1e;
     `;
 
     const Li = styled.li`
-        background-color: whitesmoke;
+        background-color: #222222;
         padding: 12px;
+        padding-left: 19px;
         display: block;
         align-items: center;
         justify-content: center;
         min-height: 90px;
         height: 20%;
         border-radius: 10px;
+        margin-top: 20px;
+        color: #f1ffff;
     `;
 
     const Card = styled.div`
         position: relative;
-        
         z-index: 2;
         padding: 16px 0;
-        background-color: #E3E7F1;
+        background-color: #433b1e;
+        margin-left: 30px;
         grid-row: 1 / span 2;
     `;
 
     const Card2 = styled.div`
         position: relative;
-        
         z-index: 2;
         padding: 16px 37px 0 0;
-        background-color: #E3E7F1;
+        background-color: #433b1e;
         grid-column: 2 / span 2;
-        border-left: 3px solid whitesmoke;
+        margin-bottom: 10px;
+        border-left: 3px solid #433b1e;
     `;
 
     const A = styled.a`
         text-decoration: none;
-        color: black;
-    `
+        color: #f1ffff;
+    `;
+
+    const Img = styled.img`
+        width: 50%;
+        height: auto;
+    `;
 
     const [mainNews, setMainNews] = useState([]);
     const [leftBar, setLeftbar] = useState([]);
-    const url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=6978740ff320434a9ef38606f359cbb2&pagesize=10"
-    const urlLeft = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=6978740ff320434a9ef38606f359cbb2";
+    const url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=6978740ff320434a9ef38606f359cbb2&pagesize=39"
+    const urlLeft = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=6978740ff320434a9ef38606f359cbb2&pagesize=10";
 
     useEffect(() => {
        axios.get(urlLeft)
@@ -62,7 +70,7 @@ function MainNewsList() {
             
         })
     }, [urlLeft])
-    console.dir(mainNews);
+
     useEffect(() => {
         axios.get(url)
             .then(res => {
@@ -75,17 +83,18 @@ function MainNewsList() {
             <Card>
                 <div>
                     {
-                    mainNews.map(news => <Li key={news.title}> <A href={news.url}>{news.title}</A></Li>)
+                    mainNews.map(news => <Li key={news.title}> <A href={news.url}> {news.title != null ? news.title : news.description }</A></Li>)
                     }
                 </div>
             </Card>
             <Card2>
                 <div>
                     {
-                    leftBar.map(news => <Li key={news.title}> <h3><A href={news.url}>{news.title}</A></h3><br/>
-                               <p>{news.content}</p>
-                               <p>{news.description}</p>
-                               <span>{news.publishedAt}</span>
+                    leftBar.map(news =>
+                                <Li key={news.title}> <h3><A href={news.url}>{news.title}</A></h3>
+                                {news.urlToImage !== null ? <Img alt="" src={news.urlToImage}/> : ""}
+                                <p>{news.description}</p>
+                                <span>{news.publishedAt.replace("T", " ").replace("Z", "").replaceAll("-", "/")}</span>
                                 </Li>)
                     }
                 </div>
