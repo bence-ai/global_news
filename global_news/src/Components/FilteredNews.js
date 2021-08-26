a2117ce8aa5c7e1f45356b74a3818687e6fc0873
feherdaniel <feherdaniel2010@gmail.com>
2021-08-26 12:11:42 +0200

    style: add style

--- global_news/src/Components/FilteredNews.js
+++ global_news/src/Components/FilteredNews.js
@@ -1,10 +1,70 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import styled from 'styled-components'
 
-const FilteredNews = ( {language, searchTerm, page} ) => {
+const FilteredNews = ( {language, searchTerm} ) => {
+
+    const A = styled.a`
+        text-decoration: none;
+        color: #f1ffff;
+        font-size: 30px;
+        `;
+
+    const Button = styled.button`
+        background-color: rgba(0, 0, 0, 0);
+        border: 2px solid #f1ffff;
+        border-radius: 10px;
+        color: #f1ffff;
+        height: 45px;
+        width: 100px;
+        margin-left: 43px;
+        font-size: 20px;
+        &:hover {
+            background-color: #222222;
+        }
+    `
+
+    const Button2 = styled.button`
+        background-color: rgba(0, 0, 0, 0);
+        border: 2px solid #f1ffff;
+        border-radius: 10px;
+        color: #f1ffff;
+        height: 45px;
+        width: 100px;
+        margin-left: 7px;
+        font-size: 20px;
+        &:hover {
+            background-color: #222222;
+        }
+    `
+
+    const Li = styled.li`
+        background-color: #222222;
+        padding: 12px;
+        display: block;
+        align-items: center;
+        justify-content: center;
+        min-height: 90px;
+        height: 20%;
+        border-radius: 10px;
+        text-align: center;
+        margin-top: 20px;
+        margin-right: 43px;
+        color: #f1ffff;
+    `;
+
     const [news, setNews] = useState([]);
+    const [page, setPage] = useState(1);
     const url = "https://newsapi.org/v2/everything?q=" + searchTerm + "&pagesize=10&language=" + language + "&page=" + page + "&apiKey=6978740ff320434a9ef38606f359cbb2";
 
+    const nextPage = () => {
+        setPage(page > 5 ? 6 : page + 1);
+      }
+      
+    const prevPage = () => {
+    setPage(page > 1 ? page - 1 : 1);
+    }
+    
     useEffect(() => {
         axios.get(url)
             .then(res => {
@@ -14,9 +74,11 @@ const FilteredNews = ( {language, searchTerm, page} ) => {
 
     return (
         <div>
+            <Button onClick={prevPage}>Prev</Button>
+            <Button2 onClick={nextPage}>Next</Button2>
             <ul>
             {
-            news.map(piece => <li><a key={piece.title} href={piece.url}>{piece.title}</a></li>)
+            news.map(piece => <Li><A key={piece.title} href={piece.url}>{piece.title}</A></Li>)
             }
             </ul>
         </div>
