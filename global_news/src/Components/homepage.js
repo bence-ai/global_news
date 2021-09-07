d29c4c2229a34af703e27b5d56337f6013327a88
feherdaniel <feherdaniel2010@gmail.com>
2021-09-07 11:53:32 +0200

    refactor: move styled components outside of function body

--- global_news/src/Components/homepage.js
+++ global_news/src/Components/homepage.js
@@ -2,72 +2,76 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
 
+const Main = styled.div`
+    position: relative;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-column-gap: 32px;
+    border: 1px solid transparent;
+    border-top: 1px solid #433b1e;
+    border-bottom: 1px solid #DADCE0;
+    overflow: hidden;
+    background-color: #433b1e;
+`;
+
+const Li = styled.li`
+    background-color: #222222;
+    padding: 12px;
+    padding-left: 19px;
+    display: block;
+    align-items: center;
+    justify-content: center;
+    min-height: 90px;
+    height: 20%;
+    border-radius: 10px;
+    margin-top: 20px;
+    color: #f1ffff;
+`;
+
+const Card = styled.div`
+    position: relative;
+    z-index: 2;
+    padding: 16px 0;
+    background-color: #433b1e;
+    margin-left: 30px;
+    grid-row: 1 / span 2;
+`;
+
+const Card2 = styled.div`
+    position: relative;
+    z-index: 2;
+    padding: 16px 37px 0 0;
+    background-color: #433b1e;
+    grid-column: 2 / span 2;
+    margin-bottom: 10px;
+    border-left: 3px solid #433b1e;
+`;
+
+const A = styled.a`
+    text-decoration: none;
+    color: #f1ffff;
+`;
+
+const Img = styled.img`
+    width: 50%;
+    height: auto;
+`;
+
+function formatDate(s) {
+    const date = new Date(s);
+    return date.toLocaleDateString() + " " + date.toLocaleTimeString();
+}
+
 function MainNewsList() {
-    const Main = styled.div`
-        position: relative;
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
-        grid-column-gap: 32px;
-        border: 1px solid transparent;
-        border-top: 1px solid #433b1e;
-        border-bottom: 1px solid #DADCE0;
-        overflow: hidden;
-        background-color: #433b1e;
-    `;
-
-    const Li = styled.li`
-        background-color: #222222;
-        padding: 12px;
-        padding-left: 19px;
-        display: block;
-        align-items: center;
-        justify-content: center;
-        min-height: 90px;
-        height: 20%;
-        border-radius: 10px;
-        margin-top: 20px;
-        color: #f1ffff;
-    `;
-
-    const Card = styled.div`
-        position: relative;
-        z-index: 2;
-        padding: 16px 0;
-        background-color: #433b1e;
-        margin-left: 30px;
-        grid-row: 1 / span 2;
-    `;
-
-    const Card2 = styled.div`
-        position: relative;
-        z-index: 2;
-        padding: 16px 37px 0 0;
-        background-color: #433b1e;
-        grid-column: 2 / span 2;
-        margin-bottom: 10px;
-        border-left: 3px solid #433b1e;
-    `;
-
-    const A = styled.a`
-        text-decoration: none;
-        color: #f1ffff;
-    `;
-
-    const Img = styled.img`
-        width: 50%;
-        height: auto;
-    `;
-
     const [mainNews, setMainNews] = useState([]);
     const [leftBar, setLeftbar] = useState([]);
     const url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=faad73f4c2ea4060b5f47d0867ee3a23&pagesize=39"
-    const urlLeft = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=faad73f4c2ea4060b5f47d0867ee3a23&pagesize=10";
+    const urlLeft = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=faad73f4c2ea4060b5f47d0867ee3a23&pagesize=9";
 
     useEffect(() => {
        axios.get(urlLeft)
         .then(res => {
             setLeftbar(res.data.articles)
-            
         })
     }, [urlLeft])
 
@@ -94,7 +98,7 @@ function MainNewsList() {
                                 <Li key={news.title}> <h3><A href={news.url}>{news.title}</A></h3>
                                 {news.urlToImage !== null ? <Img alt="" src={news.urlToImage}/> : ""}
                                 <p>{news.description}</p>
-                                <span>{news.publishedAt.replace("T", " ").replace("Z", "").replaceAll("-", "/")}</span>
+                                <span>{formatDate(news.publishedAt)}</span>
                                 </Li>)
                     }
                 </div>
